d0428a5066bab69a13956aff60f1ed729d3fffe5
wangyhub <459152081@qq.com>
2019-07-12 15:53:45 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,127 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    show: ''
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          console.log("res=" + res);
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log("e="+e)
+    app.globalData.userInfo = e.detail.userInfo;
+    wx.login({
+      success: function (res) {
+        //获取登录的临时凭证
+        var code = res.code;
+        //调用后端，获取微信的session_key, secret
+        wx.request({
+          url: 'http://192.168.12.94:8081/wjdc/a/mobile/tbMobile/wxLogin?code=' + code,
+          data: e.detail.userInfo,
+          method: 'POST',
+          success: function (result) {
+            var openId = result.data.data;
+            wx.setStorageSync("openId", openId);
+          }
+        })
+      }
+    })
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  click: function () {
+    var that = this;
+    var show;
+    wx.scanCode({
+      success: (res) => {
+        var questionCode = res.result;
+        var openId = wx.getStorageSync("openId");
+        console.log("questionCode=" + questionCode + "      openId=" + openId);
+        wx.request({
+          url: 'http://192.168.12.94:8081/wjdc/a/mobile/tbMobile/releQuenaire?openId=' + openId + '&queCode=' + questionCode,
+          method: 'POST',
+          success: function (result) {
+            var r = result.data;
+            if (r.status=='200'){
+              var questionnaire = result.data.data;
+              console.log("questionnaire=" + questionnaire);
+              wx.setStorageSync("quesiotnnaireId", questionnaire.id);
+              wx.redirectTo({
+                url: '../wjdc/wjdc',
+              })
+            } else if (r.status == '1'){
+              wx.showToast({
+                title: r.msg,
+                icon: 'none',
+                duration: 2000
+              })
+            }else{
+              wx.showToast({
+                title: '系统繁忙，稍后再试',
+                icon: 'none',
+                duration: 2000
+              })
+            }
+            
+          }
+        })
+        
+        
+      },
+      fail: (res) => {
+        wx.showToast({
+          title: '失败',
+          icon: 'none',
+          duration: 2000
+        })
+      },
+      complete: (res) => {
+      }
+    })
+  },
+  wjdc: function () {
+    wx.navigateTo({
+      url: '../wjdc/wjdc',
+    })
+  }
+})
---
+++ pages/wjdc/wjdc.js
@@ -0,0 +1,205 @@
+// pages/wjdc/wjdc.js
+var jsonUtil = require('../../utils/json.js');
+var app = getApp()
+Page({
+  
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    quesnaireInfo: [{
+      id: '',
+      questionName: '',
+      projectName: '',
+      questionList: [{
+        questionId: '',
+        questionTitle: '',
+        orderNum: '',
+        questionType: '',
+        optionalList: [{
+          optionalId: '',
+          optionalAnswer: '',
+          orderNum: '',
+          checked: ''
+        }]
+      }]
+    }],
+    questionAnswer: [{
+      quesionId: '',
+      optionalId: '',
+      content: ''
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var openId = wx.getStorageSync("openId");
+    var that = this;
+    wx.request({
+      url: 'http://192.168.12.94:8081/wjdc/a/mobile/tbMobile/showQuenaire?openId=' + openId,
+      method: 'POST',
+      success: function (result) {
+        var questionnaire = result.data.data;
+        that.setData({
+          quesnaireInfo: questionnaire
+        })
+      }
+    })
+  },
+
+  /**
+   * 单选题
+   */
+  radiochange: function (e) {
+    var selQuesionId = e.target.id;
+    var selOptionalId = e.detail.value;
+    var newArray = [{
+      quesionId: selQuesionId,
+      optionalId: selOptionalId
+    }]
+    //取出data中queRadio数据
+    var dataQueRadio = this.data.questionAnswer;
+    this.saveOrUpdateData('1', dataQueRadio, newArray);
+  },
+
+  /**
+   * 问答题和单选题输入框
+   */
+  anwserInput: function (e) {
+    var selQuesionId = e.target.id;
+    var selContent = e.detail.value;
+    var newArray = [{
+      quesionId: selQuesionId,
+      content: selContent
+    }]
+    //取出data中queRadio数据
+    var dataqueRadioInput = this.data.questionAnswer;
+    this.saveOrUpdateData('2', dataqueRadioInput, newArray);
+  },
+
+  formSubmit: function (e) {
+    var subData = e.detail.value;
+    //表单校验
+    if (!this.checkForm(subData)){
+      return false;
+    }
+    var openId = wx.getStorageSync("openId");
+    var questionnaireId = wx.getStorageSync("quesiotnnaireId");
+    var reqData = {
+      openId: openId,
+      questionnaireId: questionnaireId,
+      questionAnswer: this.data.questionAnswer
+    } 
+    
+    wx.request({
+      url: 'http://192.168.12.94:8081/wjdc/a/mobile/tbMobile/saveAnswer',
+      data: reqData,
+      method: 'POST',
+      success: function (result) {
+        if('200'==result.data.status){
+          wx.showToast({
+            title: '提交完成',
+            icon: 'success',
+            duration: 2000
+          })
+          wx.redirectTo({
+            url: '../index/index',
+          })
+        }else{
+          wx.showToast({
+            title: '系统繁忙，请稍后再试',
+            icon: 'none',
+            duration: 2000
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 添加或者修改data数据操作
+   */
+  saveOrUpdateData: function (type, anwserData, newArray) {
+    var selQuesionId = newArray[0].quesionId;
+    var selOptionalId = newArray[0].optionalId;
+    var selContent = newArray[0].content;
+    //取出data中queRadio数据
+    //var dataQueRadio = this.data.queRadio;
+    //如果为空修改为当前选中的
+    if (anwserData[0].quesionId == "") {
+      this.update(newArray);
+    } else {
+      //循环查询是否有重复的questionId
+      for (var i = 0; i < anwserData.length; i++) {
+        //如果有重复的questionId就修改
+        if (anwserData[i].quesionId == selQuesionId) {
+          if (type == '1') {
+            anwserData[i].optionalId = selOptionalId;
+          } else if (type == '2') {
+            anwserData[i].content = selContent;
+          }
+          this.update(anwserData);
+        } else {
+          this.save(anwserData, newArray);
+        }
+      }
+    }
+  },
+
+  /**
+   * 添加
+   */
+  save: function (data, newArray) {
+    this.setData({
+      questionAnswer: data.concat(newArray)
+    });
+  },
+
+  /**
+   * 修改
+   */
+  update: function (data) {
+    this.setData({
+      questionAnswer: data
+    });
+  },
+
+  /**
+   * 表单校验
+   */
+  checkForm: function (subData){
+    var aa = jsonUtil.jsonToString(subData);
+    var b = jsonUtil.jsonToMap(aa);
+    var showFalse = '0';
+    var showMsg = '';
+    var questionList = this.data.quesnaireInfo.questionList;
+    for (var i = 0; i < questionList.length; i++) {
+      var question = questionList[i];
+      var radioKey = 'radio' + question.questionId;
+      var inputKey = 'input' + question.questionId;
+      //判断选择题的是否答了
+      if (question.questionType == '1' && b.get(radioKey).length == 0) {
+        showFalse = '1';
+        showMsg = '请选择第' + (i + 1) + '题后再提交';
+        break;
+      }
+      //判断问答题是否答了
+      if (question.questionType == '2' && b.get(inputKey).length == 0) {
+        showFalse = '1';
+        showMsg = '请回答第' + (i + 1) + '题后再提交';
+        break;
+      }
+    }
+    if (showFalse == '1') {
+      wx.showToast({
+        title: showMsg,
+        icon: 'none',
+        duration: 2000
+      })
+      return false;
+    }
+    return true;
+  }
+})
